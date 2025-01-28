e9e80916a8dfb201262a366ccf5e1024a435ee51
GammachisT <gammachistesfaye@gmail.com>
2025-01-28 06:03:34 -0800

    Final project

--- music-player/src/components/Sidebar.jsx
+++ music-player/src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ const Sidebar = () => {
     const navigate = useNavigate();
 
   return (
-    <div className='w-[45%] h-full p-1  flex-col gap-2 text-white  lg:flex'>
+    <div className='w-[20%] h-full p-1  flex-col gap-2 text-white  lg:flex'>
             <div className='bg-[#121212] h-[15%] rounded flex flex-col justify-around'>
                 <div onClick={()=>navigate('/')} className='flex items-center gap-3 pl-8 cursor-pointer'>
                     <img className='w-6 pt-5' src={assets.home_icon} alt="" />
@@ -32,7 +32,7 @@ const Sidebar = () => {
                         <img className='w-5' src={assets.plus_icon} alt="" />
                     </div>
                 </div>
-                    <div className='h-[87%] p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4'>
+                    <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4'>
                         <h1>Create your first song playlist</h1>
                         < FileUpload />
                     </div>
